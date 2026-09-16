57122f692439b6c2de5fc03a3ef516e48c381cd8
Fernando Pastor <fpastor@abionica.com>
2026-09-16 13:07:36 +0200

    add unit tests for AverageCalculator and MSTest configuration

---
+++ Chapter01/Averages/Averages.Tests/WhenCalculatingAverages.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+namespace Averages.Tests;
+
+[TestClass]
+public class WhenCalculatingAverages
+{
+    [TestMethod]
+    public void SingleInputShouldProduceSameValueAsResult()
+    {
+        string[] inputs = { "1" };
+        double result = AverageCalculator.ArithmeticMean(inputs);
+        Assert.AreEqual(1.0, result, 1E-14);
+    }
+    [TestMethod]
+    public void MultipleInputsShouldProduceAverageAsResult()
+    {
+        string[] inputs = { "1", "2", "3" };
+        double result = AverageCalculator.ArithmeticMean(inputs);
+        Assert.AreEqual(2.0, result, 1E-14);
+    }
+    [TestMethod]
+    public void NoInputShouldThrowArgumentException()
+    {
+        string[] inputs = { };
+        Assert.Throws<ArgumentException>(() => AverageCalculator.ArithmeticMean(inputs));
+    }
+}
